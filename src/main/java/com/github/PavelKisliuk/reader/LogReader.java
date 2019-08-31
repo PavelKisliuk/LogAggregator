25cac6cfa11e7558d32e481f3a982cd9c33f47f1
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-31 12:49:06 +0300

    LogReader commented

--- src/main/java/com/github/PavelKisliuk/reader/LogReader.java
+++ src/main/java/com/github/PavelKisliuk/reader/LogReader.java
@@ -1,3 +1,7 @@
+/*  By Pavel Kisliuk, 31.08.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
 package com.github.PavelKisliuk.reader;
 
 import com.github.PavelKisliuk.model.Log;
@@ -13,18 +17,52 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+/**
+ * The {@code LogReader} class is {@code Runnable} realization for
+ * reading log's from files.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
 public class LogReader implements Runnable {
 	/**
 	 * Log extension.
 	 */
 	private static final String EXTENSION = ".log";
+
+	/**
+	 * Current number of file to reading.
+	 */
 	private static AtomicInteger fileNumber;
 
+	/**
+	 * Full amount of log files in directory for reading.
+	 */
 	private final int fileAmount;
+
+	/**
+	 * Name of directory with log files.
+	 */
 	private String directory;
+
+	/**
+	 * Name pattern of log files.
+	 */
 	private String fileName;
+
+	/**
+	 * Container for transfigured log's.
+	 */
 	private HashMap<Integer, List<Log>> logGroup = new HashMap<>();
 
+	/**
+	 * Constructor for data initialization.
+	 * <p>
+	 *
+	 * @param directory of log files.
+	 * @param fileName  is name pattern of log files.
+	 */
 	public LogReader(String directory, String fileName) {
 		this.directory = directory;
 		this.fileName = fileName;
@@ -32,11 +70,35 @@ public class LogReader implements Runnable {
 		fileAmount = Objects.requireNonNull(new File(directory).listFiles()).length;
 	}
 
+	/**
+	 * Return size of log container. Size is equal amount of log files in directory.
+	 * <p>
+	 *
+	 * @return size of log container.
+	 */
+	public int size() {
+		return logGroup.size();
+	}
+
+	/**
+	 * Return element of log container.
+	 * <p>
+	 *
+	 * @param key for choose of element.
+	 * @return element of log container.
+	 */
+	public List<Log> get(Integer key) {
+		return logGroup.get(key);
+	}
+
+	/**
+	 * Read log's from specify file, transfigure it to {@code Log} instance and put to
+	 * log container.
+	 */
 	@Override
 	public void run() {
 		int currentNumber;
 		while ((currentNumber = fileNumber.getAndIncrement()) <= fileAmount) {
-			System.out.println(directory + fileName + currentNumber + EXTENSION);
 			try (BufferedReader reader = Files.newBufferedReader(Paths.get(
 					directory + fileName + currentNumber + EXTENSION))) {
 				logGroup.put(currentNumber, reader.lines().map(Log::new).collect(Collectors.toList()));
